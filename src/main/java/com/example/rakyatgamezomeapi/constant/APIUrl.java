5f7dacbac3d096ed15795cd5a19c76662312eccc
Mochammad Mirza Kharisma <mirzamochammad49@gmail.com>
2024-09-09 14:49:26 +0700

    wip: add docker for build

--- src/main/java/com/example/rakyatgamezomeapi/constant/APIUrl.java
+++ src/main/java/com/example/rakyatgamezomeapi/constant/APIUrl.java
@@ -1,14 +1,14 @@
 package com.example.rakyatgamezomeapi.constant;
 
 public class APIUrl {
-    public static final String AUTH_API = "/api/auth";
-    public static final String TAGS_API = "/api/tags";
-    public static final String USER_API = "/api/user";
-    public static final String POST_API = "/api/post";
-    public static final String COMMENT_API = "/api/comment";
-    public static final String VOTE_POSTS_API = "/api/vote-posts";
-    public static final String VOTE_COMMENTS_API = "/api/vote-comments";
-    public static final String FOLLOW_API = "/api/follow";
-    public static final String PRODUCT_COIN_API = "/api/product-coin";
-    public static final String TRANSACTION_API = "/api/transaction";
+    public static final String AUTH_API = "/api/v1/auth";
+    public static final String TAGS_API = "/api/v1/tags";
+    public static final String USER_API = "/api/v1/user";
+    public static final String POST_API = "/api/v1/post";
+    public static final String COMMENT_API = "/api/v1/comment";
+    public static final String VOTE_POSTS_API = "/api/v1/vote-posts";
+    public static final String VOTE_COMMENTS_API = "/api/v1/vote-comments";
+    public static final String FOLLOW_API = "/api/v1/follow";
+    public static final String PRODUCT_COIN_API = "/api/v1/product-coin";
+    public static final String TRANSACTION_API = "/api/v1/transaction";
 }
